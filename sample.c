007e258b93c19388fd66e213ff1f4bb9d5db42db
NareshreddyBarrolla <naresh.mtech.ai@gmail.com>
2015-09-22 18:47:42 +0530

    ok

---
+++ sample.c
@@ -0,0 +1,14 @@
+
+# Please enter the commit message for your changes. Lines starting
+# with '#' will be ignored, and an empty message aborts the commit.
+# On branch master
+# Your branch is up-to-date with 'origin/master'.
+#
+# Changes to be committed:
+#	new file:   largeNum.c
+#	new file:   largeNum.c~
+#	new file:   sample.c
+#
+# Untracked files:
+#	sample.c~
+#
